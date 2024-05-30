4c0175b50120197e3db8eae05084f90c58685ed3
WebRizen <webrizen@gmail.com>
2024-05-30 20:33:11 +0530

    "Updated DashboardLayout and Navbar components with new functionality and styling"

--- frontend/user/src/app/dashboard/layout.js
+++ frontend/user/src/app/dashboard/layout.js
@@ -1,3 +1,5 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import {
   Home,
@@ -10,7 +12,7 @@ import {
   Briefcase,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
+import { useUserContext } from "@/context/UserContext";
 import RouteGuard from "@/components/providers/RouteGuard";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import {
@@ -20,8 +22,43 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import CommonBreadcrumbs from "@/components/system/CommonBreadcrumbs";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function DashboardLayout({ children }) {
+  const { user } = useUserContext();
+  const [userData, setUserData] = useState(null);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  // Function to fetch user data from the API
+  const loadUserData = async (userId) => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASEURL}/auth/${userId}`
+      );
+      const userResponse = response.data;
+      setUserData(userResponse);
+    } catch (error) {
+      toast.error(`Error loading user data - ${error.message}`);
+      console.error("Error loading user data:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (user && user.userId) {
+      loadUserData(user.userId);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(timer);
+  }, []);
+
   const navItems = [
     {
       href: "/dashboard",
@@ -55,7 +92,39 @@ export default function DashboardLayout({ children }) {
       tooltip: "Proposals",
     },
   ];
-
+  const navItemsClients = [
+    {
+      href: "/dashboard",
+      icon: <Home className="h-5 w-5" />,
+      label: "Dashboard",
+      tooltip: "Dashboard",
+    },
+    {
+      href: "/dashboard/orders",
+      icon: <ShoppingCart className="h-5 w-5" />,
+      label: "Orders",
+      tooltip: "Orders",
+      className: "bg-accent text-accent-foreground",
+    },
+    {
+      href: "/dashboard/workspace",
+      icon: <Workflow className="h-5 w-5" />,
+      label: "Workspace",
+      tooltip: "Workspace",
+    },
+    {
+      href: "/dashboard/projects",
+      icon: <Briefcase className="h-5 w-5" />,
+      label: "Projects",
+      tooltip: "Projects",
+    },
+    {
+      href: "/dashboard/proposals",
+      icon: <WalletCards className="h-5 w-5" />,
+      label: "Proposals",
+      tooltip: "Proposals",
+    },
+  ];
   function getGreeting() {
     const now = new Date();
     const hour = now.getHours();
@@ -73,6 +142,7 @@ export default function DashboardLayout({ children }) {
 
   return (
     <RouteGuard>
+      <Toaster position="bottom-center" />
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex bg-white">
           <TooltipProvider>
@@ -84,20 +154,45 @@ export default function DashboardLayout({ children }) {
                 <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
                 <span className="sr-only">Miragelancer</span>
               </Link>
-              {navItems.map(({ href, icon, tooltip, className = "" }) => (
-                <Tooltip key={tooltip}>
-                  <TooltipTrigger asChild>
-                    <Link
-                      href={href}
-                      className={`flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 ${className}`}
-                    >
-                      {icon}
-                      <span className="sr-only">{tooltip}</span>
-                    </Link>
-                  </TooltipTrigger>
-                  <TooltipContent side="right">{tooltip}</TooltipContent>
-                </Tooltip>
-              ))}
+              {user.role === "freelancer" ? (
+                <>
+                  {navItems.map(({ href, icon, tooltip, className = "" }) => (
+                    <Tooltip key={tooltip}>
+                      <TooltipTrigger asChild>
+                        <Link
+                          href={href}
+                          className={`flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 ${className}`}
+                        >
+                          {icon}
+                          <span className="sr-only">{tooltip}</span>
+                        </Link>
+                      </TooltipTrigger>
+                      <TooltipContent side="right">{tooltip}</TooltipContent>
+                    </Tooltip>
+                  ))}
+                </>
+              ) : user.role === "client" ? (
+                <>
+                  {navItemsClients.map(
+                    ({ href, icon, tooltip, className = "" }) => (
+                      <Tooltip key={tooltip}>
+                        <TooltipTrigger asChild>
+                          <Link
+                            href={href}
+                            className={`flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 ${className}`}
+                          >
+                            {icon}
+                            <span className="sr-only">{tooltip}</span>
+                          </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">{tooltip}</TooltipContent>
+                      </Tooltip>
+                    )
+                  )}
+                </>
+              ) : (
+                <>Please Login</>
+              )}
             </nav>
             <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
               <Tooltip>
@@ -133,16 +228,37 @@ export default function DashboardLayout({ children }) {
                     <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
                     <span className="sr-only">Miragelancer</span>
                   </Link>
-                  {navItems.map(({ href, icon, label, className = "" }) => (
-                    <Link
-                      key={label}
-                      href={href}
-                      className={`flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground ${className}`}
-                    >
-                      {icon}
-                      {label}
-                    </Link>
-                  ))}
+                  {user.role === "freelancer" ? (
+                    <>
+                      {navItems.map(({ href, icon, label, className = "" }) => (
+                        <Link
+                          key={label}
+                          href={href}
+                          className={`flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground ${className}`}
+                        >
+                          {icon}
+                          {label}
+                        </Link>
+                      ))}
+                    </>
+                  ) : user.role === "client" ? (
+                    <>
+                      {navItemsClients.map(
+                        ({ href, icon, label, className = "" }) => (
+                          <Link
+                            key={label}
+                            href={href}
+                            className={`flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground ${className}`}
+                          >
+                            {icon}
+                            {label}
+                          </Link>
+                        )
+                      )}
+                    </>
+                  ) : (
+                    <>Please Login</>
+                  )}
                 </nav>
               </SheetContent>
             </Sheet>
@@ -150,13 +266,13 @@ export default function DashboardLayout({ children }) {
             <div className="relative ml-auto md:flex hidden justify-end gap-2 items-center whitespace-nowrap">
               <span>
                 {getGreeting()},{" "}
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-600">
-                  Arshahdul Ahmed
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-600 font-bold">
+                  {userData?.displayName || "N/A"}
                 </span>
               </span>
               <span>•</span>
               <span className="text-slate-500">
-                {new Date().toLocaleString()}
+                {currentTime.toLocaleString()}
               </span>
             </div>
           </header>
