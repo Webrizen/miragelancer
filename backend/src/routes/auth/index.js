4c841f12fee6b5aa022176ec4dd93b0e42bd6834
WebRizen <webrizen@gmail.com>
2024-06-25 14:09:57 +0530

    backend started again

--- backend/src/routes/auth/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-router.use("/consumer", require("./consumerAuth"));
-
-router.use("/freelancer", require("./freelancerAuth"));
-
-router.use("/admin", require("./adminAuth"));
-
-module.exports = router;
