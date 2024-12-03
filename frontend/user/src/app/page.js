f58324ff8d6689df81106db9317b57bc931296dd
WebRizen <135026315+Webrizen@users.noreply.github.com>
2024-12-03 17:15:07 +0530

    Update page.js

--- frontend/user/src/app/page.js
+++ frontend/user/src/app/page.js
@@ -4,51 +4,51 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-            <div
-              className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+      <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+        <div
+          className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+            }}
+          />
+        </div>
+        <div
+          className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+            }}
+          />
+        </div>
+        <div className="flex flex-wrap items-center justify-center w-full text-center gap-x-4 gap-y-2">
+          <p className="text-sm leading-6 text-gray-900">
+            <strong className="font-semibold">Version 1.2</strong>
+            <svg
+              viewBox="0 0 2 2"
+              className="mx-2 inline h-0.5 w-0.5 fill-current"
               aria-hidden="true"
             >
-              <div
-                className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-                style={{
-                  clipPath:
-                    "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-                }}
-              />
-            </div>
-            <div
-              className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-              aria-hidden="true"
-            >
-              <div
-                className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-                style={{
-                  clipPath:
-                    "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-                }}
-              />
-            </div>
-            <div className="flex flex-wrap items-center w-full text-center gap-x-4 gap-y-2">
-              <p className="text-sm leading-6 text-gray-900">
-                <strong className="font-semibold">Version 1.2</strong>
-                <svg
-                  viewBox="0 0 2 2"
-                  className="mx-2 inline h-0.5 w-0.5 fill-current"
-                  aria-hidden="true"
-                >
-                  <circle cx={1} cy={1} r={1} />
-                </svg>
-                We're still developing the website, please come back later.
-              </p>
-              <a
-                href="mailto:webrizen@gmail.com"
-                className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-              >
-                Become Early Investor <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            We're still developing the website, please come back later.
+          </p>
+          <a
+            href="mailto:webrizen@gmail.com"
+            className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          >
+            Become Early Investor <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </div>
       <div className="relative isolate px-6 lg:px-8 min-h-screen flex flex-col justify-center items-center overflow-hidden">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -80,11 +80,11 @@ export default function Home() {
               Are you ready to experience the future of freelancing? Welcome to Miragelancer, where the power of AI meets the world of freelancing to bring you a revolutionary platform tailored for the new age of work.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Link href="/">
-              <Button aschild>
-                Get started
+              <Button asChild>
+                <Link href="/">
+                  Get started
+                </Link>
               </Button>
-              </Link>
               <Link
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
